4704bf7643b5e9c92f850a383ad0f9cf519c8054
Tanveer Hoque <thaque20@gmail.com>
2019-10-22 18:55:27 +0600

    Updates

---
+++ precache-manifest.5694b0f0a97805244d3a9b43a6e2e1b1.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f1ff0393f108cc7b26f73e6807d4a14",
+    "url": "/qhunterweb/index.html"
+  },
+  {
+    "revision": "76c20aa05ccac9a460b7",
+    "url": "/qhunterweb/static/css/2.1f49d22b.chunk.css"
+  },
+  {
+    "revision": "33b65b1e3af343977371",
+    "url": "/qhunterweb/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "76c20aa05ccac9a460b7",
+    "url": "/qhunterweb/static/js/2.513ba624.chunk.js"
+  },
+  {
+    "revision": "33b65b1e3af343977371",
+    "url": "/qhunterweb/static/js/main.7bd7ae4c.chunk.js"
+  },
+  {
+    "revision": "63c7ae0607e2b708710b",
+    "url": "/qhunterweb/static/js/runtime-main.f57a3033.js"
+  },
+  {
+    "revision": "2dca1bc2e9a6a2ce83c41782425b73cb",
+    "url": "/qhunterweb/static/media/app-logo-small.2dca1bc2.png"
+  },
+  {
+    "revision": "4e8b017aef78cc114006cba896850fbd",
+    "url": "/qhunterweb/static/media/dropify.4e8b017a.eot"
+  },
+  {
+    "revision": "86cb1ab018f1680d8e0fa0ca6a53d64c",
+    "url": "/qhunterweb/static/media/dropify.86cb1ab0.ttf"
+  },
+  {
+    "revision": "950ffd1efa29c33a70dd86ea761cb3d8",
+    "url": "/qhunterweb/static/media/dropify.950ffd1e.woff"
+  },
+  {
+    "revision": "b321d818cc153b16e739072ea8bc94b8",
+    "url": "/qhunterweb/static/media/dropify.b321d818.svg"
+  }
+]);
